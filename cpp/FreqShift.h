7a7140ad9e39cdcafdba0ef42196e3305f23bea8
Matt Ntiros <matt.ntiros@axiosengineering.com>
2015-02-10 16:28:47 -0500

    Contains updates to FreqShift component

--- cpp/FreqShift.h
+++ cpp/FreqShift.h
@@ -17,16 +17,20 @@ public:
 	~FreqShift_i();
 	int serviceFunction();
 
+
 private:
 	double sampleRate;
 	vector<float> data;
+	bool firstTime;	//indicates whether or not current iteration of the service function is the first
 
-
+	//Function passes three vectors (two input vectors (in1 and in2) and one output vector (d))
+	//and computes the product of the corresponding elements of in1 and in2. Template allows
+	//compatibility with multiple data types
 	template<typename T, typename U, typename V>
-	void dotmultiply(vector<T> &in1, vector<U> &in2, vector<V> &d)
+	void vectormultiply(vector<T> &in1, vector<U> &in2, vector<V> &out)
 	{
 		typedef typename Calc<T, U>::output_type typeIneed;
-		vector<typeIneed> *castedData = (vector<typeIneed>*)&d;
+		vector<typeIneed> *castedData = (vector<typeIneed>*)&out;
 		castedData->resize(in1.size());
 		for(unsigned int i=0;i<castedData->size();i++)
 		{
